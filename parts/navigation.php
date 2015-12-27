72ee785419fa82ebf9a038cb9050ae2461e4bfc7
Marty Spellerberg <marty@halfempty.com>
2015-12-27 14:41:09 -0800

    Nav tweak

--- parts/navigation.php
+++ parts/navigation.php
@@ -4,7 +4,7 @@
 		<ul>
 			<li <?php if ( is_etc_section('work') ) echo 'class="active"'; ?>><a href="/everything">Work</a></li>
 			<li <?php if ( is_etc_section('news') ) echo 'class="active"'; ?>><a href="/category/news">News</a></li>
-			<li <?php if ( is_etc_section('about') ) echo 'class="active"'; ?>><a href="/profile">About</a></li>
+			<li <?php if ( is_etc_section('about') ) echo 'class="active"'; ?>><a href="/about">About</a></li>
 		</ul>
 	</div>
 
